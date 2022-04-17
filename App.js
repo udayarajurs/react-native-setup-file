4c4964d204d6529bff1d9c70f0645ba96303bae5
udayarajurs <udayarajurs7@gmail.com>
2022-04-17 13:23:42 +0530

    k

--- App.js
+++ App.js
@@ -3,17 +3,25 @@ import {View, Text, StyleSheet, StatusBar} from 'react-native';
 import {colors} from './src/global/styles';
 import RootNavigator from './src/navigation/RootNavigator';
 import SplashScreen from 'react-native-splash-screen';
+//redux
+import {Provider} from 'react-redux';
+import {store, appPersist} from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View style={styles.container}>
-      {/* <StatusBar barStyle="light-content" backgroundColor={colors.statusbar} /> */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={appPersist}>
+        <View style={styles.container}>
+          {/* <StatusBar barStyle="light-content" backgroundColor={colors.statusbar} /> */}
 
-      <RootNavigator />
-    </View>
+          <RootNavigator />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/screens/SearchScreeen.js
+++ src/screens/SearchScreeen.js
@@ -1,14 +1,170 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  FlatList,
+  Image,
+} from 'react-native';
 
-const SearchScreeen = navigation => {
+import {connect} from 'react-redux';
+import {fetchMovies, addToWishList, removeFromWishlist} from '../redux/actions';
+import {BASE_URL} from '../utilities';
+
+const SearchScreeen = props => {
+  const {movieReducer, fetchMovies, addToWishList, removeFromWishlist} = props;
+
+  const {movies, wishlist} = movieReducer;
+
+  const [currentMovie, setCurrentMovie] = useState(undefined);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      setCurrentMovie(movies[0]);
+    }
+  }, [movies]);
+
+  const didTapCurrentMovie = movie => {
+    setCurrentMovie(movie);
+  };
+
+  const onTapAddToWishlist = movie => {
+    addToWishList(movie);
+  };
+
+  const onTapRemoveFromWishlist = movie => {
+    removeFromWishlist(movie);
+  };
+
+  const isExist = movie => {
+    if (wishlist.filter(item => item._id === movie._id).length > 0) {
+      return true;
+    }
+
+    return false;
+  };
+
   return (
     <View>
-      <Text>SearchScreeen</Text>
+      <View>
+        <FlatList
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={movies}
+          renderItem={({item}) => (
+            <View style={styles.movieCard}>
+              <TouchableOpacity
+                style={{flex: 1, justifyContent: 'space-between'}}
+                onPress={() => didTapCurrentMovie(item)}>
+                <Image
+                  resizeMode="stretch"
+                  style={{
+                    width: 25,
+                    height: 25,
+                  }}
+                  source={{
+                    uri: `${BASE_URL}/s3_images/${item.thumbnail}`,
+                  }}
+                />
+              </TouchableOpacity>
+              <Text style={{textAlign: 'center', padding: 20}}>
+                {' '}
+                {item.title}{' '}
+              </Text>
+              {isExist(item) ? (
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#208103',
+                    width: '100%',
+                    height: 44,
+                    borderBottomLeftRadius: 20,
+                    borderBottomRightRadius: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onPress={() => onTapRemoveFromWishlist(item)}>
+                  <Text
+                    style={{color: '#FFF', fontSize: 12, fontWeight: '600'}}>
+                    {' '}
+                    Remove from Wishlist
+                  </Text>
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#D92F24',
+                    width: '100%',
+                    height: 44,
+                    borderBottomLeftRadius: 20,
+                    borderBottomRightRadius: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onPress={() => onTapAddToWishlist(item)}>
+                  <Text
+                    style={{color: '#FFF', fontSize: 12, fontWeight: '600'}}>
+                    {' '}
+                    Add to Wishlist
+                  </Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
   );
 };
 
-export default SearchScreeen;
+const mapStateToProps = state => ({
+  movieReducer: state.movieReducer,
+});
 
-const styles = StyleSheet.create({});
+const ASearchScreeen = connect(mapStateToProps, {
+  fetchMovies,
+  addToWishList,
+  removeFromWishlist,
+})(SearchScreeen);
+
+export default ASearchScreeen;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#E5E5E5',
+  },
+  posterView: {
+    display: 'flex',
+    width: Dimensions.get('screen').width,
+    flex: 7,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  listView: {
+    width: Dimensions.get('screen').width,
+    flex: 5,
+    padding: 10,
+  },
+  poster: {
+    display: 'flex',
+    width: Dimensions.get('screen').width,
+    height: '100%',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+  },
+  movieCard: {
+    height: 150,
+    width: 200,
+  },
+});
